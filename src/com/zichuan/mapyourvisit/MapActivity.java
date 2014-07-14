1fd8dcb819c51f084010605bf0b71867799af08b
Jerry <chem.dexter@gmail.com>
2014-07-14 01:04:53 -0400

    First commit. Implemented a Google Map fragment.

---
+++ src/com/zichuan/mapyourvisit/MapActivity.java
@@ -0,0 +1,35 @@
+package com.zichuan.mapyourvisit;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends FragmentActivity {
+	private GoogleMap mMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		setUpMapIfNeeded();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
+
+	private void setUpMapIfNeeded() {
+		if (mMap != null) {
+			return;
+		}
+		mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+		if (mMap == null) {
+			return;
+		}
+
+	}
+}
